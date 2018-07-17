8eff7f715490ce936ee0c627a54d980e6f5c96dc
johnnywebs <cpcount1@gmail.com>
2018-07-17 23:27:31 +0800

    New Login Page + Files
    New Design for Login Page

--- admin/application/views/login.php
+++ admin/application/views/login.php
@@ -1,130 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
 
+<!doctype html>
+<html class="no-js " lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!-- Favicon icon -->
-    <!--<link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">-->
-    <title>Geo Grout - Login</title>
-    <!-- Bootstrap Core CSS -->
-    <link href="<?php echo base_url("public/assets/plugins/bootstrap/css/bootstrap.min.css");?>" rel="stylesheet">
-    <!-- Custom CSS -->
-     <link href="<?php echo base_url("public/css/style.css"); ?>" rel="stylesheet">
-    <!-- You can change the theme colors from here -->
-     <link href="<?php echo base_url("public/css/colors/blue.css"); ?>" id="theme" rel="stylesheet">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
-</head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<meta name="description" content="">
 
-<body>
-    <!-- ============================================================== -->
-    <!-- Preloader - style you can find in spinners.css -->
-    <!-- ============================================================== -->
-    <div class="preloader">
-        <svg class="circular" viewBox="25 25 50 50">
-            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
-    </div>
-    <!-- ============================================================== -->
-    <!-- Main wrapper - style you can find in pages.scss -->
-    <!-- ============================================================== -->
-    <section id="wrapper" class="login-register login-sidebar" style="background-image:url(<?php echo base_url('public/assets/images/Geogrout_DSC_0289_1920.jpg'); ?>);">
-        <div class="login-box card rounded-0">
-            <div class="card-body">
-                <form class="form-horizontal form-material" id="loginform" method="get" action="<?php echo base_url("employees/dashboard"); ?>">
-                    <a href="javascript:void(0)" class="text-center db"><img src="<?php echo base_url("public/assets/images/logo.png");?>" alt="Home" /></a>
-                    <div class="form-group m-t-40">
-                        <div class="col-xs-12">
-                            <input class="form-control" type="text" required="" placeholder="Username">
+<title>GeoGrout - Login</title>
+<!-- Favicon-->
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="<?php echo base_url("public/assets/plugins/bootstrap/css/bootstrap.min.css");?>">
+
+<!-- Custom Css -->
+<link rel="stylesheet" href="<?php echo base_url("public/css/main.css");?>">    
+<link rel="stylesheet" href="<?php echo base_url("public/css/color_skins.css");?>">
+</head>
+<body class="theme-black">
+<div class="authentication">
+    <div class="container">
+        <div class="col-md-12 content-center">
+            <div class="row">
+                <div class="col-lg-4 col-md-12">
+                                     
+                </div>
+                <div class="col-lg-4 col-md-12">
+                    <div class="card-plain">
+                        <div class="header">
+                            <!--<h5>Geo Grout Login</h5>-->
+                            <img src="<?php echo base_url("public/assets/images/logo.png"); ?>" />
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-xs-12">
-                            <input class="form-control" type="password" required="" placeholder="Password">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-12">
-                            <div class="checkbox checkbox-primary pull-left p-t-0">
-                                <input id="checkbox-signup" type="checkbox">
-                                <label for="checkbox-signup"> Remember me ?</label>
+                        <form class="form">
+                            <div class="input-group">
+                                <input type="email" class="form-control" placeholder="Email">
+                                <span class="input-group-addon"><i class="zmdi zmdi-account-circle"></i></span>
                             </div>
-                            <a href="javascript:void(0)" id="to-recover" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a> </div>
-                    </div>
-                    <div class="form-group text-center m-t-20">
-                        <div class="col-xs-12">
-                            <button onclick="document.location.href='<?php echo base_url("employees/dashboard"); ?>';" class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Log In</button>
+                            <div class="input-group">
+                                <input type="password" placeholder="Password" class="form-control" />
+                                <span class="input-group-addon"><i class="zmdi zmdi-lock"></i></span>
+                            </div>                            
+                        </form>
+                        <div class="footer">
+                            <a href="<?php echo base_url("employees/dashboard"); ?>" class="btn btn-primary btn-round btn-block">SIGN IN</a>
                         </div>
+                        <!--<a href="forgot-password.html" class="link">Forgot Password?</a>-->
                     </div>
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-12 col-md-12 m-t-10 text-center">
-                            <div class="social"><a href="javascript:void(0)" class="btn  btn-facebook" data-toggle="tooltip" title="Login with Facebook"> <i aria-hidden="true" class="fa fa-facebook"></i> </a> <a href="javascript:void(0)" class="btn btn-googleplus" data-toggle="tooltip" title="Login with Google"> <i aria-hidden="true" class="fa fa-google-plus"></i> </a> </div>
-                        </div>
-                    </div>
-                    <div class="form-group m-b-0">
-                        <div class="col-sm-12 text-center">
-                            <p>Don't have an account? <a href="#" class="text-primary m-l-5"><b>Sign Up</b></a></p>
-                        </div>
-                    </div>
-                </form>
-                <form class="form-horizontal" id="recoverform" action="index.html">
-                    <div class="form-group ">
-                        <div class="col-xs-12">
-                            <h3>Recover Password</h3>
-                            <p class="text-muted">Enter your Email and instructions will be sent to you! </p>
-                        </div>
-                    </div>
-                    <div class="form-group ">
-                        <div class="col-xs-12">
-                            <input class="form-control" type="text" required="" placeholder="Email">
-                        </div>
-                    </div>
-                    <div class="form-group text-center m-t-20">
-                        <div class="col-xs-12">
-                            <button class="btn btn-primary btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Reset</button>
-                        </div>
-                    </div>
-                </form>
+                </div>
+				<div class="col-lg-4 col-md-12">
+			
+				</div>
             </div>
         </div>
-    </section>
-    <!-- ============================================================== -->
-    <!-- End Wrapper -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- All Jquery -->
-    <!-- ============================================================== -->
-    <script src="<?php echo base_url("public/assets/plugins/jquery/jquery.min.js");?>"></script>
-    <!-- Bootstrap tether Core JavaScript -->
-    <script src="<?php echo base_url("public/assets/plugins/bootstrap/js/popper.min.js");?>"></script>
-    <script src="<?php echo base_url("public/assets/plugins/bootstrap/js/bootstrap.min.js");?>"></script>
-    <!-- slimscrollbar scrollbar JavaScript -->
-	<script src="<?php echo base_url("public/js/jquery.slimscroll.js");?>"></script>
-    <!--Wave Effects -->
-    <script src="<?php echo base_url("public/js/waves.js");?>"></script>
-    <!--Menu sidebar -->
-    <script src="<?php echo base_url("public/js/sidebarmenu.js");?>"></script>
-    <!--stickey kit -->
-    <script src="<?php echo base_url("public/assets/plugins/sticky-kit-master/dist/sticky-kit.min.js");?>"></script>
-    <script src="<?php echo base_url("public/assets/plugins/sparkline/jquery.sparkline.min.js");?>"></script>
-    <!--Custom JavaScript -->
-    <script src="<?php echo base_url("public/js/custom.min.js");?>"></script>
-    <!-- ============================================================== -->
-    <!-- Style switcher -->
-    <!-- ============================================================== -->
-   <script src="<?php echo base_url("public/assets/plugins/styleswitcher/jQuery.style.switcher.js");?>"></script>
+    </div>
+</div>
 </body>
-<script>
-		$(document).ready(function() {
-			$(".preloader").fadeOut();
-		});
-	</script>
 </html>
